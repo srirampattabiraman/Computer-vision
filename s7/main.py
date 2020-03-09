fe7a015c1e4e5210262c637210cde272132bd10d
srirampattabiraman <55537646+srirampattabiraman@users.noreply.github.com>
2020-03-09 14:43:02 +0530

    Update main.py

--- s7/main.py
+++ s7/main.py
@@ -1,1 +1,9 @@
 
+## Importing the Libraries 
+
+from __future__ import print_function
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from torchvision import datasets, transforms
